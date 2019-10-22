af21b93f55e9aa10250b619ab0dee78517442309
Viorel GURDIS <vgurdis@clj.bitdefender.biz>
2019-10-22 14:49:43 +0300

    DirectoryMonitor service added

---
+++ DirectoryMonitoring/DirectoryMonitoring/DirectoryMonitoringService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoryMonitoring
+{
+    public partial class DirectoryMonitoringService : ServiceBase
+    {
+        protected FileSystemWatcher Watcher;
+
+        private string PathToFolder = @"D:\";
+
+        public DirectoryMonitoringService()
+        {
+            Logger.Instance.LoggerPath = @"D:\";
+            Logger.Instance.LoggerFileName = "DirectoryMonitoring";
+            Watcher = new MyFileSystemWatcher(PathToFolder);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
---
+++ DirectoryMonitoring/DirectoryMonitoring/MyFileSystemWatcher.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoryMonitoring
+{
+    public class MyFileSystemWatcher : FileSystemWatcher
+    {
+        public MyFileSystemWatcher()
+        {
+            Init();
+        }
+
+        public MyFileSystemWatcher(string directoryPath) : base(directoryPath)
+        {
+            Init();
+        }
+
+        public MyFileSystemWatcher(string directoryPath, string filter)
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            IncludeSubdirectories = true;
+            NotifyFilter = NotifyFilters.FileName | NotifyFilters.Size;
+            EnableRaisingEvents = true;
+            Created += Watcher_Created;
+        }
+
+        private void Watcher_Created(object sender, FileSystemEventArgs e)
+        {
+            Logger.WriteLine("File created or added: " + e.FullPath);
+        }
+    }
+}
