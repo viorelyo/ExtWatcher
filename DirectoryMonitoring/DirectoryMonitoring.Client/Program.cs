471d6b70ff9fcf9ff14e7fdf91eafa71b06e191f
Viorel Gurdis <vgurdis@bitdefender.com>
2019-11-03 23:07:39 +0200

    DirectoryMonitoring Client added

---
+++ DirectoryMonitoring/DirectoryMonitoring.Client/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoryMonitoring.Client
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //InstanceContext site = new InstanceContext();
+        }
+    }
+}
