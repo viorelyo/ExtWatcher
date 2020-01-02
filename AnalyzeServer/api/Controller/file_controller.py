5801cbdecd49273e60a61255ef6a2e7bbe54d9ad
viorelyo <viorelgurdis@gmail.com>
2020-01-02 12:49:48 +0200

    More REST Methods added to AnalyzeServer

--- AnalyzeServer/api/Controller/file_controller.py
+++ AnalyzeServer/api/Controller/file_controller.py
@@ -7,18 +7,17 @@ from Common.constants import *
 
 
 class FileController:
-    def __init__(self, repo, file):
+    def __init__(self, repo):
         self.repo = repo
-        self.file = file
         self.file_path_to_be_analyzed = None
         self.analyzer = None
 
-    def save_uploaded_file(self):
-        filename = secure_filename(self.file.filename)
+    def save_uploaded_file(self, file):
+        filename = secure_filename(file.filename)
         self.file_path_to_be_analyzed = os.path.join(UPLOAD_FOLDER, filename)
 
         app.logger.info("Saving uploaded file: '{}".format(self.file_path_to_be_analyzed))
-        self.file.save(self.file_path_to_be_analyzed)
+        file.save(self.file_path_to_be_analyzed)
         self.repo.add_file(filename, self.file_path_to_be_analyzed)
 
     def analyze(self):
@@ -28,3 +27,9 @@ class FileController:
         is_malicious = self.analyzer.process()
         time.sleep(10)
         return FILE_STATUS_MALICIOUS if is_malicious else FILE_STATUS_BENIGN
+
+    def get_all_analyzed_files(self):
+        return self.repo.get_all_files()
+
+    def get_analyzed_file(self, filename):
+        return self.repo.get_file(filename)
--- AnalyzeServer/api/main.py
+++ AnalyzeServer/api/main.py
@@ -27,8 +27,8 @@ def upload_file():
         response.status_code = 400
         return response
     elif file and validator.allowed_file(file.filename):
-        file_controller = FileController(repo, file)
-        file_controller.save_uploaded_file()
+        file_controller = FileController(repo)
+        file_controller.save_uploaded_file(file)
         file_status = file_controller.analyze()
 
         response = jsonify({'message': file_status})
@@ -38,6 +38,39 @@ def upload_file():
         message = 'Allowed file types are: [{}]'.format(', '.join(ext for ext in ALLOWED_EXTENSIONS))
 
         response = jsonify({'message': message})
+        response.status_code = 400
+        return response
+
+
+@app.route('/api/analyzed-files', methods=['GET'])
+def get_files():
+    app.logger.info("GET Method: analyzed-files was triggered.")
+    file_controller = FileController(repo)
+    files = file_controller.get_all_analyzed_files()
+
+    response = jsonify({'files': files})
+    response.status_code = 200
+    return response
+
+
+@app.route('/api/analyzed-file', methods=['GET'])
+def get_file_by_name():
+    app.logger.info("GET Method: analyzed-file was triggered.")
+
+    filename = request.args.get('filename')
+    if filename:
+        file_controller = FileController(repo)
+        file = file_controller.get_analyzed_file(filename)
+
+        if file:
+            response = jsonify({'file': file})
+            response.status_code = 200
+        else:
+            response = jsonify({"message": "Filename not found."})
+            response.status_code = 404
+        return response
+    else:
+        response = jsonify({"message": "Request does not contain filename parameter."})
         response.status_code = 400
         return response
 
