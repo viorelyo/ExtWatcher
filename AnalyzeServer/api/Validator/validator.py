179a9ee9ebe2353b7426a98a142b38f853931633
viorelyo <viorelgurdis@gmail.com>
2020-03-01 14:16:52 +0200

    Validate_url implemented

--- AnalyzeServer/api/Validator/validator.py
+++ AnalyzeServer/api/Validator/validator.py
@@ -1,3 +1,5 @@
+import requests
+from urllib.parse import urlparse
 from Common.constants import ALLOWED_EXTENSIONS
 
 
@@ -9,5 +11,25 @@ class Validator:
         return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
     def validate_url(self, submitted_url):
-        #TODO implement url validation + maybe check if url is available
-        return False
+        if submitted_url == "":
+            return False
+        if not self.__check_url(submitted_url):
+            return False
+        return self.__check_connection(submitted_url)
+
+    def __check_url(self, url):
+        try:
+            result = urlparse(url)
+            return all([result.scheme, result.netloc, result.path])
+        except:
+            return False
+
+    def __check_connection(self, url):
+        try:
+            request = requests.get(url)
+            if request.status_code == 200:
+                return True
+            else:
+                return False
+        except ConnectionError:
+            return False
