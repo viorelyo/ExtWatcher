a50e1e56e35e2c0830ad9c606fa99d374592c3e0
viorelyo <viorelgurdis@gmail.com>
2020-03-07 18:58:13 +0200

    Feed sorted by datetime

--- AnalyzeServer/api/Repository/FeedRepository/feed_repository.py
+++ AnalyzeServer/api/Repository/FeedRepository/feed_repository.py
@@ -11,11 +11,12 @@ class FeedRepository:
         self.query = Query()
         app.logger.info("FeedRepository initialized.")
 
-    def add_event(self, event_type, submitted_url, origin_ip):
-        app.logger.info("Adding new event. Event Type: '{}'; Submitted-Url: '{}'; Origin-IP: '{}'".format(event_type, submitted_url, origin_ip))
+    def add_event(self, event_type, content, origin_ip):
+        app.logger.info("Adding new event. Event Type: '{}'; Content: '{}'; Origin-IP: '{}'".format(event_type, content, origin_ip))
         server_current_time = time.strftime("%d/%m/%Y - %H:%M:%S")
-        self.feed.insert({"datetime": server_current_time, "event_type": event_type.lower(), "submitted_url": submitted_url, "origin_ip": origin_ip})
+        self.feed.insert({"datetime": server_current_time, "event_type": event_type.lower(), "content": content, "origin_ip": origin_ip})
 
     def get_feed(self):
         app.logger.info("Getting all events from feed.")
-        return self.feed.all()
+        a = self.feed.all()
+        return sorted(a, key=lambda r: time.strptime(r['datetime'], "%d/%m/%Y - %H:%M:%S"), reverse=True)
