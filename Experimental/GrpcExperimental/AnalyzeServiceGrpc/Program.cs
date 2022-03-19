a310b6e2733adb2fc534f906ebfb15521610ed23
viorelyo <viorelgurdis@gmail.com>
2022-03-19 17:23:55 +0200

    GrpcActor added && HTTP/3 connection established

--- Experimental/GrpcExperimental/AnalyzeServiceGrpc/Program.cs
+++ Experimental/GrpcExperimental/AnalyzeServiceGrpc/Program.cs
@@ -1,4 +1,6 @@
 using AnalyzeServiceGrpc.Services;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,10 +9,24 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.WebHost.ConfigureKestrel((context, options) =>
+{
+    options.Listen(IPAddress.Any, 5001, listenOptions =>
+    {
+        listenOptions.Protocols = HttpProtocols.Http3;
+        //listenOptions.Protocols = HttpProtocols.Http1AndHttp2AndHttp3;
+        listenOptions.UseHttps();
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
 app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
---
+++ Experimental/GrpcExperimental/GrpcActor/Program.cs
@@ -0,0 +1,30 @@
+﻿using Grpc.Net.Client;
+using GrpcActor;
+using System.Net;
+
+
+var handler = new Http3Handler(new HttpClientHandler());
+
+var channel = GrpcChannel.ForAddress("https://localhost:5001", new GrpcChannelOptions() { HttpHandler = handler });
+var client = new Greeter.GreeterClient(channel);
+
+var response = await client.SayHelloAsync(new HelloRequest { Name = "World" });
+
+Console.WriteLine(response.Message);
+Console.ReadKey();
+
+
+public class Http3Handler : DelegatingHandler
+{
+    public Http3Handler() { }
+    public Http3Handler(HttpMessageHandler innerHandler) : base(innerHandler) { }
+
+    protected override Task<HttpResponseMessage> SendAsync(
+        HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        request.Version = HttpVersion.Version30;
+        request.VersionPolicy = HttpVersionPolicy.RequestVersionExact;
+
+        return base.SendAsync(request, cancellationToken);
+    }
+}
