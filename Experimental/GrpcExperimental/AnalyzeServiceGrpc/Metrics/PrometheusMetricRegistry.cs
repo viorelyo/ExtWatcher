6946a308069ac232b3c003a1ddcb5f1d155ea058
viorelyo <viorelgurdis@gmail.com>
2022-05-25 11:47:32 +0300

    grpc grafana dashboard created

--- Experimental/GrpcExperimental/AnalyzeServiceGrpc/Metrics/PrometheusMetricRegistry.cs
+++ Experimental/GrpcExperimental/AnalyzeServiceGrpc/Metrics/PrometheusMetricRegistry.cs
@@ -36,14 +36,14 @@ namespace AnalyzeServiceGrpc.Metrics
         private static readonly Histogram GrpcCallsDuration = Prometheus.Metrics.CreateHistogram("grpc_call_duration_milliseconds", "Histogram of gRPC calls processing durations.", 
             new HistogramConfiguration
             { 
-                Buckets = new[] { 5.0, 10.0, 25.0, 50.0, 100.0, 200.0, 500.0, 800.0, 1000.0, 1200.0, 1400.0, 1600.0, 1800.0, 2000.0, 2300.0, 2600.0, 2900.0, 3100.0, 3400.0, 3800.0, 4200.0, 4600.0, 5000.0, 5500.0, 5800.0, 6200.0 },
+                Buckets = new[] { 1.0, 2.0, 4.0, 8.0, 16.0, 32.0, 64.0, 128.0, 256.0, 512.0, 1024.0, 2048.0, 4096.0, 8192.0, 16364.0, 32728.0 },
                 LabelNames = new[] { "method" }
             });
 
         private static readonly Histogram HttpCallsDuration = Prometheus.Metrics.CreateHistogram("http_call_duration_milliseconds", "Histogram of http calls processing durations.",
             new HistogramConfiguration
             {
-                Buckets = new[] { 5.0, 10.0, 25.0, 50.0, 100.0, 200.0, 500.0, 800.0, 1000.0, 1200.0, 1400.0, 1600.0, 1800.0, 2000.0, 2300.0, 2600.0, 2900.0, 3100.0, 3400.0, 3800.0, 4200.0, 4600.0, 5000.0, 5500.0, 5800.0, 6200.0 },
+                Buckets = new[] { 1.0, 2.0, 4.0, 8.0, 16.0, 32.0, 64.0, 128.0, 256.0, 512.0, 1024.0, 2048.0, 4096.0, 8192.0, 16364.0, 32728.0 },
                 LabelNames = new[] { "method", "endpoint" }
             });
 
