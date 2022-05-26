a1d055860dc08228373d48394c128099513991ba
viorelyo <viorelgurdis@gmail.com>
2022-05-26 13:57:00 +0300

    FileHasher tool created

---
+++ Experimental/Tools/FileHasher/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography;
+
+try
+{
+    using var md5 = MD5.Create();
+
+    using var readStream = File.OpenRead(args[0]);
+    var fileHash = BitConverter.ToString(md5.ComputeHash(readStream)).Replace("-", "");
+
+    Environment.Exit(0);
+}
+catch
+{
+    Environment.Exit(1);
+}
